423aefd50b00df606cf817c92a08c3bfc40ae151
Xin <torresfranky30@gmail.com>
2021-04-06 20:03:32 -0500

    Vista Tabla

--- src/routes/links.js
+++ src/routes/links.js
@@ -1,10 +1,15 @@
 const express = require('express');
-const routes = express.Router();
+const router = express.Router();
 
 const pool = require('../datebase');
 
-routes.get('/add', (req, res) => {
+router.get('/add', (req, res) => {
     res.render('links/add');
 });
 
-module.exports = routes;
+router.post('/add', (req, res) => {
+    console.log(req.body);
+    res.send('Recibido');
+});
+
+module.exports = router;
