e3d8486aa9ab952230f039d23efc9e4c3ebf2132
Xin <torresfranky30@gmail.com>
2021-04-07 02:29:32 -0500

    Mostrando datos

--- src/routes/links.js
+++ src/routes/links.js
@@ -7,9 +7,25 @@ router.get('/add', (req, res) => {
     res.render('links/add');
 });
 
-router.post('/add', (req, res) => {
-    console.log(req.body);
+router.post('/add', async (req, res) => {
+    const { DNI, Nombres, ApellidoP, ApellidoM, correo, telefono } = req.body;
+    const newLinks = {
+
+        Nombres,
+        ApellidoP,
+        ApellidoM,
+        DNI,
+        correo,
+        telefono
+    };
+    await pool.query('INSERT INTO personas set ?', [newLinks]);
+    //console.log(newLinks);
     res.send('Recibido');
+});
+
+router.get('/add', async (req, res) => {
+    const listapersonas = await pool.query('SELECT*FROM personas');
+    res.render('links/add', { listapersonas });
 });
 
 module.exports = router;
